0bc06467558ea41d2d19da6cb4d877689428878d
JesusXD88 <71974251+JesusXD88@users.noreply.github.com>
2025-09-23 17:55:02 +0200

    Fixed broken rebase

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -8,17 +8,8 @@ iso_application="DolphinOS installation ISO"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d.%H.%M)"
 install_dir="dolphinos"
 buildmodes=('iso')
-<<<<<<< HEAD
-<<<<<<< HEAD
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
-=======
-bootmodes=('uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
->>>>>>> f07a72e (Set and configure SystemD-boot)
-=======
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
->>>>>>> ace9c53 (Add bios boot so kernel is added to live system :()
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
